fc5069e5525babff5464772f4254e815d19c86db
NealMaze <57574975+NealMaze@users.noreply.github.com>
2019-11-09 15:48:53 -0700

    removed dial class def

---
+++ dial_def.py
@@ -0,0 +1,43 @@
+#class
+class dial:
+    def __init__(
+    self,
+    name,
+    point,
+    click,
+    team1,
+    rang,
+    bolts,
+    star,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    eleven,
+    twelve,
+    ):
+        self.name = name
+        self.point = point
+        self.click = click
+        self.team1 = team1
+        self.rang = rang
+        self.bolts = bolts
+        self.star = star
+        self.one = one
+        self.two = two
+        self.three = three
+        self.four = four
+        self.five = five
+        self.six = six
+        self.seven = seven
+        self.eight = eight
+        self.nine = nine
+        self.ten = ten
+        self.eleven = eleven
+        self.twelve = twelve
